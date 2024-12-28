bd1e710db5ac4345e9c87cf61d9ac313d4b6abe0
baobao0303 <baobao22.work@gmail.com>
2024-12-29 01:17:50 +0700

    feat: Add set, update, and mutate methods for signal management

--- 40_Angular_Signals/src/app/signals/signals.component.ts
+++ 40_Angular_Signals/src/app/signals/signals.component.ts
@@ -8,16 +8,18 @@ import { Component, signal } from "@angular/core";
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
 
   counter = signal(0);
 
   increment() {
-    this.counter.update((oldValue) => oldValue + 1);
-    this.actions.push("INCREMENT");
+    // this.counter.update((oldValue) => oldValue + 1);
+    // this.actions.push("INCREMENT");
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions) => oldActions.push("INCREMENT"));
   }
   decrement() {
-    this.counter.update((oldValue) => oldValue - 1);
-    this.actions.push("DECREMENT");
+    this.counter.set(this.counter() - 1);
+    this.actions.mutate((oldActions) => oldActions.push("DECREMENT"));
   }
 }
