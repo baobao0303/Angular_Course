e5a1b4d638a5651059eb32aeab4257448a35c3bf
baobao0303 <baobao22.work@gmail.com>
2024-12-28 23:58:21 +0700

    feat: Implement signal value update mechanism

--- 40_Angular_Signals/src/app/signals/signals.component.ts
+++ 40_Angular_Signals/src/app/signals/signals.component.ts
@@ -13,11 +13,11 @@ export class SignalsComponent {
   counter = signal(0);
 
   increment() {
-    this.counter++;
+    this.counter.update((oldValue) => oldValue + 1);
     this.actions.push("INCREMENT");
   }
   decrement() {
-    this.counter--;
+    this.counter.update((oldValue) => oldValue - 1);
     this.actions.push("DECREMENT");
   }
 }
