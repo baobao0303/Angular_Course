e9bd9531af0fc402208ddd422101df77e9557392
baobao0303 <baobao22.work@gmail.com>
2024-12-29 10:34:26 +0700

    feat: Implement data reading from the store

--- 41_NgRx/src/app/store/store.reducer.ts
+++ 41_NgRx/src/app/store/store.reducer.ts
@@ -2,4 +2,11 @@ import { createReducer } from '@ngrx/store';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState);
+// Cach 1
+// export const counterReducer = createReducer(initialState);
+
+// An Alternative Way Of Creating Reducers
+// Cach 2
+export function counterReducer(state = initialState) {
+  return state;
+}
