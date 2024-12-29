e642aaf4cd8754ff4d4900ef91c0832e0f0c6ec0
baobao0303 <baobao22.work@gmail.com>
2024-12-29 12:13:19 +0700

    feat: Implement manual reducer for action handling without createReducer

--- 41_NgRx/src/app/store/store.reducer.ts
+++ 41_NgRx/src/app/store/store.reducer.ts
@@ -4,14 +4,19 @@ import { createReducer, on } from '@ngrx/store';
 const initialState = 0;
 
 // Cach 1
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state, action) => state + action.value),
-  on(decrement, (state) => state - 1)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value),
+//   on(decrement, (state) => state - 1)
+// );
 
 // An Alternative Way Of Creating Reducers
 // Cach 2
-// export function counterReducer(state = initialState) {
-//   return state;
-// }
+export function counterReducer(state = initialState, action: any) {
+  if (action.type === '[Counter] increment') {
+    return state + action.value;
+  } else if (action.type === '[Counter] decrement') {
+    return state - 1;
+  }
+  return state;
+}
