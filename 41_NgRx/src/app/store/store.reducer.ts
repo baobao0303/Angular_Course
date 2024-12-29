66dce9aba48e335d0beea0f1857dbfe91911bcea
baobao0303 <baobao22.work@gmail.com>
2024-12-29 10:41:47 +0700

    feat: Add actions and reducers for counter state management

--- 41_NgRx/src/app/store/store.reducer.ts
+++ 41_NgRx/src/app/store/store.reducer.ts
@@ -1,12 +1,16 @@
-import { createReducer } from '@ngrx/store';
+import { increment } from './counter.actions';
+import { createReducer, on } from '@ngrx/store';
 
 const initialState = 0;
 
 // Cach 1
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
 // An Alternative Way Of Creating Reducers
 // Cach 2
-export function counterReducer(state = initialState) {
-  return state;
-}
+// export function counterReducer(state = initialState) {
+//   return state;
+// }
